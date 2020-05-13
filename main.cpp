2857898b63208277733f91671374ec7528e3ce1e
Jonesoez <321654987Aa@>
2020-05-14 01:56:50 +0200

    clean up

--- main.cpp
+++ main.cpp
@@ -8,16 +8,13 @@
 
 Graph G;
 
-
-
 int main()
 {
-	std::cout << "GraphY v1.1\n\n";
+	std::cout << "GraphY v1.2\n\n";
 	std::string CSVmatrix;
 	std::ifstream GraphFile("matrix.csv");
 	
 	int m_temp[100] = { 0 };
-
 
 	if (GraphFile.is_open())
 	{
@@ -38,64 +35,65 @@ int main()
 				G.Matrix[i][j] = m_temp[m_data++];
 			}
 		}
+
+		//Print Knoten
+		printf("Knoten der Adjazenzmatrix: %d\n", G.getGraphDimension());
+		G.Knoten = G.getGraphDimension();
+
+
+		//Print 2D Converted Matrix
+		printf("\n\n\nPrinting static Array Matrix:\n");
+		for (int i = 0; i < G.getGraphDimension(); i++)
+			for (int j = 0; j < G.getGraphDimension(); j++)
+				std::cout << G.Matrix[i][j] << std::setw(5);
+
+		printf("\n\n");
+
+
+		G.creatDistanzMatrix();
+		printf("\n\n\nDISTANZMATRIX:\n");
+		for (int i = 0; i < G.getGraphDimension(); i++)
+			for (int j = 0; j < G.getGraphDimension(); j++)
+				std::cout << G.DistanzMatrix[i][j] << std::setw(5);
+
+
+		printf("\n\n");
+
+		G.calcExzentrizitaet();
+		std::cout << "Exzentrizitaeten: \n";
+		for (int i = 0; i < G.getGraphDimension(); i++)
+			std::cout << G.Exzentrizitaeten[i] << std::endl;
+
+
+		G.calcDurchmesser();
+		std::cout << "Durchmesser: ";
+		std::cout << G.Durchmesser << std::endl;
+
+		G.calcRadius();
+		std::cout << "Radius: ";
+		std::cout << G.Radius << std::endl;
+
+		G.calcZentren();
+		std::cout << "Zentren: ";
+		std::cout << "An den Knoten: [ ";
+		for (int i = 0; i < G.getGraphDimension(); i++)
+		{
+			if (G.Zentren[i] != 0)
+				std::cout << i + 1 << " ";
+		}
+		std::cout << "]";
+
+
+		std::cout << "\n\n\n\n\n" << std::endl;
+
+
 		GraphFile.close();
 	}
 	else
 	{
-		std::cout << "Error: Unable to open Graph file";
+		std::cout << "Error: Unable to open Graph file \"matrix.csv\"\n\n";
 	}
 
-	//Print Knoten
-	printf("Knoten der Adjazenzmatrix: %d\n", G.getGraphDimension());
-	G.Knoten = G.getGraphDimension();
-
-
-	//Print 2D Converted Matrix
-	printf("\n\n\nPrinting static Array Matrix:\n");
-	for (int i = 0; i < G.getGraphDimension(); i++)
-		for (int j = 0; j < G.getGraphDimension(); j++)
-			std::cout << G.Matrix[i][j] << std::setw(5);
-
-	printf("\n\n");
-
-
-
-	G.creatDistanzMatrix();
-	printf("\n\n\nDISTANZMATRIX:\n");
-	for (int i = 0; i < G.getGraphDimension(); i++)
-		for (int j = 0; j < G.getGraphDimension(); j++)
-			std::cout << G.DistanzMatrix[i][j] << std::setw(5);
-
-	
-
-	printf("\n\n");
-
-	G.calcExzentrizitaet();
-	std::cout << "Exzentrizitaeten: \n";
-	for(int i = 0; i < G.getGraphDimension(); i++)
-		std::cout << G.Exzentrizitaeten[i] << std::endl;
-
-
-	G.calcDurchmesser();
-	std::cout << "Durchmesser: ";
-	std::cout << G.Durchmesser << std::endl;
-
-	G.calcRadius();
-	std::cout << "Radius: ";
-	std::cout << G.Radius << std::endl;
-
-	G.calcZentren();
-	std::cout << "Zentren: ";
-	std::cout << "An den Knoten: [ ";
-	for (int i = 0; i < G.getGraphDimension(); i++)
-	{
-		if (G.Zentren[i] != 0)
-			std::cout << i + 1 << " ";
-	}
-	std::cout << "]";
-
-
-	std::cout << "\n\n\n\n\n" << std::endl;
 	system("pause");
 	return 0;
 }
